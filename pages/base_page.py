569c37638c29cde50c2c8fb0b736371931c81f93
Vitaliy <scromnyagga@gmail.com>
2021-08-04 10:18:15 +0300

    create product tests with parametrize

--- pages/base_page.py
+++ pages/base_page.py
@@ -30,7 +30,7 @@ class BasePage():
         answer = str(math.log(abs((12 * math.sin(float(x))))))
         alert.send_keys(answer)
         alert.accept()
-        time.sleep(10)
+        time.sleep(5)
         try:
             alert = self.browser.switch_to.alert
             alert_text = alert.text
